54da5742051356c43f80e5b8f54579cf9277d865
kovner chalumo <kchalumo@gmail.com>
2018-12-25 18:44:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
 
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+}
+
